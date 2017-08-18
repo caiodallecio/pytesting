58bd26011238ce31665a0174d994f3ea1b67092a
caiodallecio <caiodallecio@gmail.com>
2017-08-18 14:41:04 -0300

    Retrieve and compare working

--- main.py
+++ main.py
@@ -2,39 +2,52 @@ import ast
 import sys
 import os.path
 import pickle as pk
+import hashlib as hs
 
 
-def get_all_functions(ast_tree):
-    return [n for n in ast.walk(ast_tree) if isinstance(n, ast.FunctionDef)]
+def get_all_nodes(ast_tree):
+    return [n for n in ast.walk(ast_tree)]
 
 
-def get_all_functions_calls(ast_tree):
-    return [n for n in ast.walk(ast_tree) if isinstance(n, ast.Call)]
+def get_all_functions(nodes):
+    return [n for n in nodes if isinstance(n, ast.FunctionDef)]
+
+
+def get_all_functions_calls(nodes):
+    return [n for n in nodes if isinstance(n, ast.Call)]
 
 
 def parse_ast(file_name):
-    with open(filename, "rt") as file:
-        return ast.parse(file.read(), filename=file_name)
+    with open(file_name, "rt") as handle:
+        return ast.parse(handle.read(), filename=file_name)
 
 
 def create_serial_object(ast_tree):
-    function_def = get_all_functions(ast_tree)
-    function_calls = get_all_functions_calls(ast_tree)
-    return [function_def, function_calls]
+    obj = []
+    hasher = hs.sha512()
+    nodes = get_all_nodes(ast_tree)
+    hasher.update(bytes(ast.dump(ast_tree), 'utf-8'))
+    ident = hasher.digest()
+    obj.append(ident)
+    obj.append(get_all_functions(nodes))
+    obj.append(get_all_functions_calls(nodes))
+    return obj
 
 
 def serialize_tree_data(file_name, ast_tree):
-    with open(file_name, 'wb') as file:
-        pk.dump(create_serial_object(ast_tree), file)
+    with open(file_name, 'wb') as handle:
+        pk.dump(create_serial_object(ast_tree), handle)
 
 
 def un_serialize_tree_data(file_name):
-    with open(file_name, 'rb') as file:
-        return pk.load(file)
+    with open(file_name, 'rb') as handle:
+        return pk.load(handle)
 
 
-def compare_ast(ast1, ast2):
-    return ast.dump(ast1) == ast.dump(ast2)
+def compare_tree_data_objects(a,b):
+    if a[0] == b[0]:
+        return True
+    return False  
 
 
 if __name__ == "__main__":
@@ -43,10 +56,10 @@ if __name__ == "__main__":
         data_name = 'data_' + filename.split('.')[0] + '.pk'
         if not os.path.isfile(data_name):
             serialize_tree_data(data_name, tree)
-        elif map(ast.dump,un_serialize_tree_data(data_name))[:] == map(ast.dump,create_serial_object(tree))[:]:
+        elif compare_tree_data_objects(create_serial_object(tree), un_serialize_tree_data(data_name)):
             print('No changes')
         else:
-            print(list(map(ast.dump,create_serial_object(tree))))
+            print('Changes')
 
 
 
