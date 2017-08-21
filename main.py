02a504468efd92b4ecb2dcde83600fd0d7985a6f
caiodallecio <caiodallecio@gmail.com>
2017-08-21 15:15:05 -0300

    commit

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sys
 import os.path
 import pickle as pk
 import hashlib as hs
+import astunparse
 
 
-class TreeData:
+
+class TreeData():
     def __init__(self, ast_tree):
         hasher = hs.sha512()
         nodes = get_all_nodes(ast_tree)
@@ -13,28 +15,73 @@ class TreeData:
         self.hash = hasher.digest()
         self.function_declarations = get_all_functions(nodes)
         self.function_calls = get_all_functions_calls(nodes)
+        self.tree = ast_tree
 
     def __eq__(self, other):
         return self.hash == other.hash
 
     def __ne__(self, other):
         return not self.__eq__(other)
+    
+    def update_test_file(self,name):
+        old_tree = TreeData(parse_ast(name))
+        new_tree = TreeData(ast.parse(self.generate_test_file(name)))
+        for nw_node in new_tree.function_declarations:
+            for od_node in old_tree.function_declarations:
+                if nw_node.name == od_node.name:
+                    nw_node.body = od_node.body
+        return astunparse.unparse(new_tree.tree)
 
     def generate_test_file(self, file_name):
-        with open(file_name, 'w') as handle:
-            module_name = file_name.split('_')[1].split('.')[0]
-            handle.writelines([
-                'import %s as module\n' % module_name,
-                'import %s\n' % 'unittest',
-                'class Test%s(unittest.TestCase):\n\t' % module_name])
+        ret = []
+
+        module_name = file_name.split('_')[1].split('.')[0]
+        ret += generate_test_imports([
+            (module_name, 'module'),
+            ('unittest', 'test'),
+            ('hypothesis','hyp')])
+        ret.append('class Test%s(test.TestCase):\n\t' % module_name)
+
+        for func in self.function_declarations:
+            ret += generate_test_function(func)
+
+
+        ret.append('if __name__ == \'__main__\':')
+        ret.append('\ttest.main()')
+        ret = '\n'.join(ret)
+        return ret
+            
+            
+
+
+def generate_test_imports(import_list):
+        ret = []
+        for imp in import_list:
+            if isinstance(imp, tuple):
+                ret.append('import %s as %s' % (imp[0], imp[1]))
+            else:
+                ret.append('import %s' % imp)
+        return ret
+
+    
+def generate_test_function(func):
+    ret = []
+    ret.append('\tdef test_%s(self):' % func.name)
+    ret.append('\t\tself.assertTrue(False)')
+    ret.append('\n')
+    return ret
 
 
 def get_all_nodes(ast_tree):
     return [n for n in ast.walk(ast_tree)]
 
+def write_test_file(file_name,data):
+    with open(file_name, 'w') as handle:
+        handle.write(data)
 
 def get_all_functions(nodes):
     return [n for n in nodes if isinstance(n, ast.FunctionDef)]
+
 
 
 def get_all_functions_calls(nodes):
@@ -54,12 +101,14 @@ def serialize_tree_data(file_name, tree_data):
 def un_serialize_tree_data(file_name):
     with open(file_name, 'rb') as handle:
         return pk.load(handle)
-
+def foo():
+    pass
 
 if __name__ == "__main__":
     for filename in sys.argv[1:]:
         tree = TreeData(parse_ast(filename))
         data_name = 'data_' + filename.split('.')[0] + '.pk'
+        test_name = 'test_' + filename.split('.')[0] + '.py'
         if not os.path.isfile(data_name):
             serialize_tree_data(data_name, tree)
             tree.generate_test_file('test_' + filename.split('.')[0] + '.py')
@@ -67,7 +116,15 @@ if __name__ == "__main__":
             print('No changes')
         else:
             print('Changes Generating new Test File')
-            tree.generate_test_file('test_' + filename.split('.')[0] + '.py')
+            
+            if os.path.isfile(test_name):
+
+                test_source = tree.update_test_file(test_name)
+            else:
+                test_source = tree.generate_test_file(test_name)
+
+            serialize_tree_data(data_name, tree)
+            write_test_file(test_name,test_source)
 
 
 
--- test_main.py
+++ test_main.py
@@ -1,4 +1,54 @@
+
 import main as module
-import unittest
-class Testmain(unittest.TestCase):
-	
+import unittest as test
+from hypothesis import given
+from hypothesis.strategies import text
+
+class Testmain(test.TestCase):
+    
+    def test_generate_test_imports(self):
+        self.assertTrue(False)
+
+    def test_generate_test_function(self):
+        self.assertTrue(False)
+
+    def test_get_all_nodes(self):
+        self.assertTrue(True)
+
+    def test_write_test_file(self):
+        self.assertTrue(False)
+
+    def test_get_all_functions(self):
+        self.assertTrue(False)
+
+    def test_get_all_functions_calls(self):
+        self.assertTrue(False)
+
+    def test_parse_ast(self):
+        self.assertTrue(False)
+
+    def test_serialize_tree_data(self):
+        self.assertTrue(False)
+
+    def test_un_serialize_tree_data(self):
+        self.assertTrue(False)
+
+    def test_foo(self):
+        self.assertTrue(False)
+
+    def test___init__(self):
+        self.assertTrue(False)
+
+    def test___eq__(self):
+        self.assertTrue(False)
+
+    def test___ne__(self):
+        self.assertTrue(False)
+
+    def test_update_test_file(self):
+        self.assertTrue(False)
+
+    def test_generate_test_file(self):
+        self.assertTrue(False)
+if (__name__ == '__main__'):
+    test.main()
